5e7e55fb91650ecd4ef64854b63263ccba2f03f2
Teegan Toliver <teegantoliver@hotmail.com>
2022-12-14 11:31:47 -0700

    button changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>An app about hating Kyle White (Demon Emoji)</h1>
+        <h1>Welcome To Teeegan's Website</h1>
         <form action="https://www.linkedin.com/in/teegan-toliver/">
           <input type="submit" value="LinkedIn" class="linked-in-button"/>
         </form>
